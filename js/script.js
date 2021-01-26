25cc5f079e00376e59cdd5aba9ec743d74c61376
dc480506 <dev.c@somaiya.edu>
2021-01-26 23:53:37 +0530

    Mashup almost done some minor bugs left

--- js/script.js
+++ js/script.js
@@ -5,19 +5,33 @@ const mapbox_URL= "pk.eyJ1IjoiZGM0ODA1MDYiLCJhIjoiY2trY2E0ZzJlMGN5YTJvcXJmNnU1Mm
 let app= angular.module("Mashup",[])
 
 app.controller("Controller",($scope,$http)=>{
-    $scope.title="Restaurants"
-    mapboxgl.accessToken = mapbox_URL
+    
+      var userCoordinates;
+      $scope.query=''
+      var geojson = {
+        type: 'FeatureCollection',
+        features: []
+      };
+// **************** Map ********************
+      var currentMarkers=[]
+      mapboxgl.accessToken = mapbox_URL
+
       var map = new mapboxgl.Map({
         container: "map", // container id
         style: "mapbox://styles/mapbox/streets-v11",
         center: [-96, 37.8], // starting position
         zoom: 3, // starting zoom
       });
-    //   var userCoordinates;
-      
-      navigator.geolocation.getCurrentPosition(position => {
-        userCoordinates = [position.coords.longitude, position.coords.latitude];
-        fetchRestaurants(userCoordinates)
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        })
+      );
+      plotMap=()=>{
+        
         map.addSource("user-coordinates", {
           type: "geojson",
           data: {
@@ -28,23 +42,73 @@ app.controller("Controller",($scope,$http)=>{
             }
           }
         });
-  
+        // map.on("load", function(e){
+        //   e.map.resize();
+        // });
         map.addLayer({
           id: "user-coordinates",
           source: "user-coordinates",
           type: "circle",
         });
+        console.log(userCoordinates)
         map.flyTo({
           center: userCoordinates,
           zoom: 14
         });
-      });
-      
-      var geojson = {
-        type: 'FeatureCollection',
-        features: []
-      };
-      fetchRestaurants=(userCoordinates)=>{
+      }
+      removeCurrentMarkers=()=>{
+        if (currentMarkers!==null) {
+          for (var i = currentMarkers.length - 1; i >= 0; i--) {
+            currentMarkers[i].remove();
+          }
+        }
+      }
+      makeMarkers=(restaurant_list)=>{
+        // var marker_tp = new mapboxgl.Marker().addTo(map);
+        // marker_tp.remove();
+        // removeCurrentMarkers();
+        geojson.features=[]
+        for(i=0;i< restaurant_list.length;i++){
+          var location= restaurant_list[i].restaurant.location
+          var name= restaurant_list[i].restaurant.name
+          var cuisines= restaurant_list[i].restaurant.cuisines
+          var rating = restaurant_list[i].restaurant.user_rating.aggregate_rating
+          geojson.features.push({
+             type: 'Feature',
+             geometry: {
+               type: 'Point',
+               coordinates: [location.longitude, location.latitude]
+             },
+             properties: {
+               title: name,
+               description: "Cuisines: "+cuisines+"<br>"+"Rating: "+rating,
+             }
+          })
+      }
+      // console.log(geojson.features)
+        geojson.features.forEach(function(marker) {
+  
+          // create a HTML element for each feature
+          var el = document.createElement('div');
+          el.className = 'marker';
+          
+          // console.log("Hello")
+          // make a marker for each feature and add to the map
+          oneMarker=new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+            .addTo(map);
+            currentMarkers.push(oneMarker)
+        });
+      }
+// **************** Fetch Restaurants**************
+      $scope.fetchRestaurants=()=>{
+        
+        // console.log($scope.query)
+        if($scope.query==''){
+          $scope.info="Showing Nearby Restaurants"
+
           $http.get(`${zomato_URL}/geocode?lon=${userCoordinates[0]}&lat=${userCoordinates[1]}`,
           {
               mode: "cors",
@@ -56,50 +120,36 @@ app.controller("Controller",($scope,$http)=>{
           .then((response)=>{
              console.log(response.data)
              $scope.restaurants=response.data.nearby_restaurants
-             $scope.top_restaurants= $scope.restaurants.slice(0,3)
-             geojson.features=[]
-             for(i=0;i<$scope.restaurants.length;i++){
-                 var location=$scope.restaurants[i].restaurant.location
-                 var name=$scope.restaurants[i].restaurant.name
-                 var cuisines= $scope.restaurants[i].restaurant.cuisines
-                 var rating = $scope.restaurants[i].restaurant.user_rating.aggregate_rating
-                 geojson.features.push({
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: [location.longitude, location.latitude]
-                    },
-                    properties: {
-                      title: name,
-                      description: "Cuisines: "+cuisines+"<br>"+"Rating: "+rating,
-                    }
-                 })
-             }
-             geojson.features.forEach(function(marker) {
+             $scope.top_restaurants= $scope.restaurants.slice(0,8)
+             makeMarkers($scope.restaurants);
+          // },(error)=>{
 
-                // create a HTML element for each feature
-                var el = document.createElement('div');
-                el.className = 'marker';
-              
-                // make a marker for each feature and add to the map
-                new mapboxgl.Marker(el)
-                  .setLngLat(marker.geometry.coordinates)
-                  .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-                  .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-                  .addTo(map);
-              });
-          },(error)=>{
+          // })
+            });
+        }
+        else{
+          $scope.info=`You searched for "${$scope.query}"`;
+          $http.get(`${zomato_URL}/search?q=${$scope.query}&count=8`,
+          {
+              mode: "cors",
+              headers:{
+                  'user-key': zomato_key
+                }
+          }
+          )
+          .then((response)=>{
+             console.log(response.data)
+             $scope.top_restaurants=response.data.restaurants
+             makeMarkers($scope.top_restaurants);
+            });
+        }
 
-          })
-      }
+    }
+
+      navigator.geolocation.getCurrentPosition(position => {
+        userCoordinates = [position.coords.longitude, position.coords.latitude];
+        $scope.fetchRestaurants()
+        plotMap()
+      });
       
-      map.addControl(
-        new mapboxgl.GeolocateControl({
-          positionOptions: {
-            enableHighAccuracy: true,
-          },
-          trackUserLocation: true,
-        })
-      );
-
-});
+  });
