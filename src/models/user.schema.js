370cff1a7e41ad8d1d7a888122a9a0d08ce050f9
dc480506 <dev.c@somaiya.edu>
2021-03-30 00:32:41 +0530

    Updated user.schema

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -21,11 +21,6 @@ var userSchema = new mongoose.Schema({
     },
     // saltSecret: String
 });
-//custom validation Email
-userSchema.path('email').validate((val) => {
-    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(val);
-}, "Invalid Email");
 
 //preEvent
 userSchema.pre('save', async function (next) {
